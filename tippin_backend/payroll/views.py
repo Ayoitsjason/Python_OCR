999caa4587f6b3746a58d1783b286a1259c6b149
Jason Le <jasonctle@gmail.com>
2023-01-11 20:33:39 -0800

    backend (feature): added starting view for scanning, installed rest framework, installed cors

--- tippin_backend/payroll/views.py
+++ tippin_backend/payroll/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.http.response import HttpResponseBadRequest, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.parsers import JSONParser
+import logging
 
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 # Create your views here.
+
+@csrf_exempt
+def scan(request):
+  if request.method == 'POST':
+    return JsonResponse("success", safe=False)
+  else:
+    return HttpResponseBadRequest("Unable to post")
+
+    
