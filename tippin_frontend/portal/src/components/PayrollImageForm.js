dc8dd5cf420021337d5c179c417185201a2c2134
Jason Le <jasonctle@gmail.com>
2023-01-19 13:03:57 -0800

    Frontend (feature): Created payroll form component to upload image

---
+++ tippin_frontend/portal/src/components/PayrollImageForm.js
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+
+function FileUploadForm() {
+  const [file, setFile] = useState(null);
+
+  const handleFileChange = (event) => {
+    setFile(event.target.files[0]);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input type="file" onChange={handleFileChange} />
+      <button type="submit">Upload</button>
+    </form>
+  );
+}
+
+export default FileUploadForm;
